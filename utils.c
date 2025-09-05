bc4870fec6cc9d25bd86b5c068fcad2df4b11ad8
Jiyan Wang <jiyawang@3-A-20.42heilbronn.de>
2025-09-05 12:47:34 +0200

    util

--- utils.c
+++ utils.c
@@ -18,7 +18,7 @@ void	push(t_stack *stack, int value)
 
 	new_node = malloc(sizeof(t_node));
 	if (!new_node)
-		return (NULL);
+		return ;
 	new_node->value = value;
 	new_node->next = stack->top;
 	stack->top = new_node;
@@ -30,7 +30,7 @@ int	pop(t_stack *stack)
 	t_node	*tmp;
 	int		value;
 
-	if (!stack || stack->top)
+	if (!stack || !stack->top)
 		return (0);
 	tmp = stack->top;
 	value = tmp->value;
@@ -42,12 +42,49 @@ int	pop(t_stack *stack)
 
 void	swap(t_stack *stack)
 {
+	t_node	*first;
+	t_node	*second;
+
+	if (!stack || !stack->top || !stack->top->next)
+		return ;
+	first = stack->top;
+	second = stack->top->next;
+	first->next = second->next;
+	second->next = first;
+	stack->top = second;
 }
 
-void	rotate(t_stack *t_stack)
+void	rotate(t_stack *stack)
 {
+	t_node	*first;
+	t_node	*last;
+
+	if (!stack || !stack->top || !stack->top->next)
+		return ;
+	first = stack->top;
+	last = stack->top;
+	while (last->next)
+		last = last->next;
+	stack->top = first->next;
+	first->next = NULL;
+	last->next = first;
 }
 
-void	veverse_rotate(t_stack *stack)
+void	reverse_rotate(t_stack *stack)
 {
+	t_node	*prev;
+	t_node	*last;
+
+	if (!stack || !stack->top || !stack->top->next)
+		return ;
+	prev = NULL;
+	last = stack->top;
+	while (last->next)
+	{
+		prev = last;
+		last = last->next;
+	}
+	prev->next = NULL;
+	last->next = stack->top;
+	stack->top = last;
 }
