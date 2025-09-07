478e6eda99addbbf344a65f2776f098d768a1c99
Fiona-87327 <jiyanw825@gmail.com>
2025-09-07 15:59:50 +0200

    0709update

--- push_swap.c
+++ push_swap.c
@@ -6,8 +6,146 @@
 /*   By: jiyan <jiyan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:50:08 by jiyan             #+#    #+#             */
-/*   Updated: 2025/09/04 20:50:11 by jiyan            ###   ########.fr       */
+/*   Updated: 2025/09/07 15:50:36 by jiyan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	i_is_number(const char *s)
+{
+	if (!s || !*s)
+		return (0);
+	if (*s == '+' || *s == '-')
+		s++;
+	if (!*s)
+		return (0);
+	while (*s)
+	{
+		if (!('0' <= *s && *s <= '9'))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+static int	str_to_int_checked(const char *s, int *out)
+{
+	long	val;
+	int		sign;
+
+	val = 0;
+	sign = 1;
+	if (!s)
+		return (0);
+	if (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s == '0')
+		s++;
+	if (!*s)
+	{
+		*out = 0;
+		return (1);
+	}
+	while (*s)
+	{
+		if (!('0' <= *s && *s <= '9'))
+			return (0);
+		val = val * 10 + (*s - '0');
+		if (sign == 1 && val > INT_MAX)
+			return (0);
+		if (sign == -1 && - val < INT_MIN)
+			return (0);
+		s++;
+	}
+	*out = (int)(val * sign);
+	return (1);
+}
+
+static int	has_dup(t_stack *stack, int value)
+{
+	t_node	*cur;
+
+	cur = stack->top;
+	while (cur)
+	{
+		if (cur->value == value)
+			return (1);
+		cur = cur->next;
+	}
+	return (0);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_node	*tmp;
+
+	if (!stack || !*stack)
+		return ;
+	while ((*stack)->top)
+	{
+		tmp = (*stack)->top;
+		(*stack)->top = tmp->next;
+		free(tmp);
+	}
+	free(*stack);
+	*stack = NULL;
+}
+
+int	stack_len(t_stack *stack)
+{
+	if (!stack)
+		return (0);
+	return (stack->size);
+}
+
+int	stack_sorted(t_stack *stack)
+{
+	t_node	*cur;
+
+	if (!stack || !stack->top)
+		return (1);
+	cur = stack->top;
+	while (cur->next)
+	{
+		if (cur->value > cur->next->value)
+			return (0);
+		cur = cur->next;
+	}
+	return (1);
+}
+
+void	init_stack_a(t_stack **a, char **values)
+{
+	int	i;
+	int	n;
+	int	val;
+
+	if (!a || !values)
+		return ;
+	*a = malloc(sizeof(t_stack));
+	if (!*a)
+		return ;
+	(*a)->size = 0;
+	(*a)->top = NULL;
+	n = 0;
+	while (values[n])
+		n++;
+	i = n - 1;
+	while (i >= 0)
+	{
+		if (!i_is_number(values[i]) || !str_to_int_checked(values[i], &val)
+			|| has_dup(*a, val))
+		{
+			ft_putstr_fd("Error\n", 2);
+			free_stack(a);
+			exit(1);
+		}
+		push(*a, val);
+		i--;
+	}
+}
--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,14 @@
 /*   By: jiyan <jiyan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 20:24:25 by jiyan             #+#    #+#             */
-/*   Updated: 2025/09/07 14:41:20 by jiyan            ###   ########.fr       */
+/*   Updated: 2025/09/07 15:43:13 by jiyan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <libft.h>
+# include "libft.h"
 # include <limits.h>
 # include <stdbool.h>
 # include <stddef.h>
@@ -37,4 +37,12 @@ void				swap(t_stack *stack);
 void				rotate(t_stack *stack);
 void				reverse_rotate(t_stack *stack);
 
+/* public helpers / sort API */
+void		sa(t_stack **a, bool print);
+void		sort_three(t_stack **a);
+void		sort_stacks(t_stack **a, t_stack **b);
+void		init_stack_a(t_stack **a, char **values);
+void		free_stack(t_stack **stack);
+int		stack_len(t_stack *stack);
+int		stack_sorted(t_stack *stack);
 #endif
